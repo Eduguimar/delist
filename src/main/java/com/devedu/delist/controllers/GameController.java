42c28c537f2af7b0f07ce816e156381114e39252
Eduardo Guimaraes <eduguimara@gmail.com>
2023-05-09 21:41:46 -0300

    GET game by id

--- src/main/java/com/devedu/delist/controllers/GameController.java
+++ src/main/java/com/devedu/delist/controllers/GameController.java
@@ -1,10 +1,11 @@
 package com.devedu.delist.controllers;
 
+import com.devedu.delist.dto.GameDTO;
 import com.devedu.delist.dto.GameMinDTO;
-import com.devedu.delist.entities.Game;
 import com.devedu.delist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 
 }
--- src/main/java/com/devedu/delist/services/GameService.java
+++ src/main/java/com/devedu/delist/services/GameService.java
@@ -1,9 +1,11 @@
 package com.devedu.delist.services;
 
+import com.devedu.delist.dto.GameDTO;
 import com.devedu.delist.dto.GameMinDTO;
 import com.devedu.delist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +15,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         var games = gameRepository.findAll();
         return games.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        var game = gameRepository.findById(id).orElse(null);
+        return new GameDTO(game);
     }
 }
